89c8eb48f819be28dc27ee9742f09dbc7ea17917
Nikhil Yadav <117060548+Nikhil-0010@users.noreply.github.com>
2025-04-03 01:05:10 +0530

    Updated project configuration and dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // reactStrictMode: false,
+  // images: { unoptimized: true },
+};
 
 export default nextConfig;
