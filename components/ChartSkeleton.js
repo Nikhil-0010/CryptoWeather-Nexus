7dbf7d8ba591270978ea4595233c9ba5e6bf8596
Nikhil-0010 <ynikhil645@gmail.com>
2025-04-04 05:03:36 +0530

    Minor skeleton ui changes

--- components/ChartSkeleton.js
+++ components/ChartSkeleton.js
@@ -4,13 +4,13 @@ export default function ChartSkeleton({ length, width }) {
         {Array.from({ length: length }).map((_, i) => (
           <div
             key={i}
-            className={`w-${width} ${i % 2 === 0 ? 'bg-neutral-700' : 'bg-neutral-600'} rounded`}
+            className={`w-${width} ${i % 2 === 0 ? 'bg-neutral-200 dark:bg-neutral-700' : 'bg-neutral-100 dark:bg-neutral-600'} rounded`}
             style={{ height: `${30 + (i % 3) * 15}%` }} // Alternating heights
           ></div>
         ))}
   
         {/* X-Axis Placeholder */}
-        <div className="absolute bottom-0 right-0 w-full h-1 bg-neutral-500 rounded"></div>
+        <div className="absolute bottom-0 right-0 w-full h-1 bg-neutral-300 dark:bg-neutral-500 rounded"></div>
       </div>
     );
   }
--- components/NewsSkeleton.js
+++ components/NewsSkeleton.js
@@ -3,10 +3,10 @@ const NewsSkeleton = React.memo(() => {
     return (
       <div className="space-y-4">
         {[...Array(3)].map((_, index) => (
-          <div key={index} className="animate-pulse p-4 border rounded-lg bg-neutral-800 space-y-3">
-            <div className="h-6 bg-neutral-700 rounded w-3/4"></div>
-            <div className="h-4 bg-neutral-600 rounded w-full"></div>
-            <div className="h-4 bg-neutral-600 rounded w-5/6"></div>
+          <div key={index} className="animate-pulse p-4 border rounded-lg dark:bg-neutral-800 space-y-3">
+            <div className="h-6 bg-neutral-200 dark:bg-neutral-700 rounded w-3/4"></div>
+            <div className="h-4 bg-neutral-100 dark:bg-neutral-600 rounded w-full"></div>
+            <div className="h-4 bg-neutral-100 dark:bg-neutral-600 rounded w-5/6"></div>
           </div>
         ))}
       </div>
